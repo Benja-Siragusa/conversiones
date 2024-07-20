247e838661ac270edfe10767b5f4891650b143bd
benjamin <benja.siragusa.2003@gmail.com>
2024-07-20 20:08:35 -0300

    program comented

--- conversiones1.java
+++ conversiones1.java
@@ -1,22 +1,51 @@
 import java.io.*;
 
 public class conversiones1 {
+
+    //main class
     public static void main(String[] ar) throws IOException {
         BufferedReader e = new BufferedReader(new InputStreamReader(System.in));
-        int a, action, base, newBase, number, operation;
+        /*variables:
+        int
+         * a=centinela value(repeats the program until the value of "a" is -1)
+         * action=determins if the program will do a convertion or an operation
+         * base=the base of the primary number in case of convertion, or the base of the numbers for the operation
+         * newBase=in convertions, the base that we will convert our primary number
+         * operation=if we do an operation, is to indicate if we will do an addition or a substraction
+        String
+         * string1=takes the value from the first number given
+         * string2=(just for operations) takes the value of the second number given
+         */
+        
+        int a, action, base, newBase, operation;
         String string1, string2;
         do {
+            //we ask if we want to do a convertion or an operation
             System.out.println("what do yo wanna do:\n1 conversion\n2 operation(+ & -)");
             action = Integer.parseInt(e.readLine());
+
+            //we ask for the base of the primary number
             System.out.print("tell me in witch base is your number 2, 8, 10 o 16: ");
             base = Integer.parseInt(e.readLine());
+
+            //if the user wants to do a convertion 
             if (action == 1) {
-                System.out.print("tell me to witch base you want to convert the number 8, 10 o 16: ");
+
+                //we ask the number to convert
+                System.out.print("tell me the number to convert: ");
+                string1=e.readLine();
+
+                //we ask to witch base we will convert our primary number
+                System.out.print("tell me to witch base you want to CONVERT the number: ");
                 newBase = Integer.parseInt(e.readLine());
+
+                //switch to determine the primary base
                 switch (base) {
+
+                    //PRIMARY BASE: BINARY BASE
                     case 2:
-                        System.out.print("tell me your binary number: ");
-                        string1 = (e.readLine());
+
+                        //switch to see to witch base do we convert the BINARY number
                         switch (newBase) {
                             case 8:
                                 System.out.println("your binary number "+string1+" in octal is: " + b2b8(string1));
@@ -32,9 +61,11 @@ public class conversiones1 {
                                 break;
                         }
                         break;
+
+                    //PRIMARY BASE: OCTAL BASE
                     case 8:
-                        System.out.print("tell me your octal number: ");
-                        string1 = (e.readLine());
+
+                        //switch to see to witch base do we convert the OCTAL number
                         switch (newBase) {
                             case 2:
                                 System.out.println("your octal number "+string1+" in binario is: " + b8b2(string1));
@@ -50,27 +81,31 @@ public class conversiones1 {
                                 break;
                         }
                         break;
+
+                    //PRIMARY BASE: DECIMAL BASE
                     case 10:
-                        System.out.print("tell me your decimal number: ");
-                        number = Integer.parseInt(e.readLine());
+
+                        //switch to see to witch base do we convert the DECIMAL number
                         switch (newBase) {
                             case 2:
-                                System.out.println("your decimal number "+number+" in binario is " + b10b2(number));
+                                System.out.println("your decimal number "+string1+" in binario is " + b10b2(Integer.parseInt(string1)));
                                 break;
                             case 8:
-                                System.out.println("your decimal number "+number+" in octal is " + b10b8(number));
+                                System.out.println("your decimal number "+string1+" in octal is " + b10b8(Integer.parseInt(string1)));
                                 break;
                             case 16:
-                                System.out.println("your decimal number "+number+" in hexadecimal is " + b10b16(number));
+                                System.out.println("your decimal number "+string1+" in hexadecimal is " + b10b16(Integer.parseInt(string1)));
                                 break;
                             default:
                                 System.out.println("un decimal solo se puede pasar a base 2, 8 o 16.");
                                 break;
                         }
                         break;
+
+                    //PRIMARY BASE: HEXADECIMAL BASE
                     case 16:
-                        System.out.print("tell me your hexadecimal number: ");
-                        string1 = (e.readLine());
+
+                        //switch to see to witch base do we convert the HEXADECIMAL number
                         switch (newBase) {
                             case 2:
                                 System.out.println("your hexadecimal number "+string1+" in binario is: " + b16b2(string1));
@@ -90,14 +125,24 @@ public class conversiones1 {
                         System.out.println("invalid base.");
                         break;
                 }
+
+            //if the user wants to do an operation
             } else if (action == 2) {
+
+                //we ask what the user wants to do, addition or substraction
                 System.out.println("what you want to do:\n1 addition\n2 substraction");
                 operation = Integer.parseInt(e.readLine());
+
+                //we ask for the 2 numbers of the operation
                 System.out.print("tell me the first number: ");
                 string1 = (e.readLine());
                 System.out.print("tell me the second number: ");
                 string2 = (e.readLine());
+
+                //witch to decide if we do an addition or a substraction depend of the before given answer
                 switch (operation) {
+
+                    //ADDITION, another switch inside to call a method depending on the base of the numbers
                     case 1:
                         switch (base) {
                             case 2:
@@ -117,6 +162,8 @@ public class conversiones1 {
                                 break;
                         }
                         break;
+
+                    //SUBSTRACTION, another switch inside to call a method depending on the base of the numbers
                     case 2:
                         switch (base) {
                             case 2:
@@ -141,12 +188,14 @@ public class conversiones1 {
                         break;
                 }
             }
+
+            //we ask if the user wnat to do another convertion or operation or if not to end the program
             System.out.print("if want to do another operation insert any number, if not insert -1: ");
             a = Integer.parseInt(e.readLine());
         } while (a != -1);
     }
 
-    // convertir de binario a otras bases:
+    // methods to convert form BINARY to other bases:
 
     static String b2b8(String a) {
         int decimal = Integer.parseInt(a, 2);
@@ -165,7 +214,7 @@ public class conversiones1 {
         return hexa;
     }
 
-    // convertir de octal a otras bases:
+    // methods to convert form OCTAL to other bases:
 
     static String b8b2(String a) {
         int decimal = Integer.parseInt(a, 8);
@@ -185,7 +234,7 @@ public class conversiones1 {
         return hexa;
     }
 
-    // convertir de decimal a otras bases:
+    // methods to convert form DECIMAL to other bases:
 
     static String b10b2(int a) {
         String binary = Integer.toBinaryString(a);
@@ -202,7 +251,7 @@ public class conversiones1 {
         return hexa;
     }
 
-    // convertir de sexadecimal a otras bases:
+    // methods to convert form HEXADECIMAL to other bases:
 
     static String b16b2(String a) {
         int decimal = Integer.parseInt(a, 16);
@@ -221,7 +270,7 @@ public class conversiones1 {
         return decimal;
     }
 
-    // suma y resta binaria:
+    // BINARY operations + & -
 
     static void suma2(String a, String b) {
         int n1 = Integer.parseInt(a, 2);
@@ -237,7 +286,7 @@ public class conversiones1 {
         System.out.println("The subtraction of the binary numbers is: \n" + a + " - " + b + " = " + binary);
     }
 
-    // suma y resta octal:
+    // OCTAL operations + & -
 
     static void suma8(String a, String b) {
         int n1 = Integer.parseInt(a, 8);
@@ -253,7 +302,7 @@ public class conversiones1 {
         System.out.println("The subtraction of the octal numbers is: \n" + a + " - " + b + " = " + octal);
     }
 
-    // suma y resta decimal:
+    // DECIMAL operations + & -
 
     static int suma10(int a, int b) {
         return a + b;
@@ -263,7 +312,7 @@ public class conversiones1 {
         return a - b;
     }
 
-    // suma y resta sexadecimal:
+    // HEXADECIMAL operations + & -
 
     static void suma16(String a, String b) {
         int n1 = Integer.parseInt(a, 16);
